32ea05b492828b6be635492cdba82fe254a94770
Brandon Sy <brandoncollegemail@gmail.com>
2019-10-08 16:41:26 -0700

    Added Basic/Brief Javadoc Comments

---
+++ AddingMachine.java
@@ -0,0 +1,48 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	/**
+     * Gets local variable total
+     * 
+     * @return total
+     */
+	public int getTotal () {
+		return 0;
+	}
+    
+    /**
+     * 
+     * @param value
+     */
+	public void add (int value) {
+		
+	}
+    
+    /**
+     * 
+     * @param value
+     */
+	public void subtract (int value) {
+		
+	}
+    
+    /**
+     * toString function for this class
+     */
+	public String toString () {
+		return "";
+	}
+
+    /**
+     * 
+     */
+	public void clear() {
+	
+	}
+}
