9167477c383c409b65742d52cd08704aa62a786a
Brandon Sy <brandoncollegemail@gmail.com>
2019-10-09 16:25:37 -0700

    junit tester add

---
+++ Testing.java
@@ -0,0 +1,21 @@
+import cse360assign2.AddingMachine;
+import jdk.internal.jline.internal.TestAccessible;
+
+public class Testing
+{
+    @Test
+    public void addingTest()
+    {
+        AddingMachine tester = new AddingMachine();
+        assertEquals(1, tester.add(1), "Adding 1 to the init should equal 1");
+        assertEquals(2, tester.add(1), "Adding 1 to current total should equal 2");
+    }
+
+    @Test
+    public void subtractingTest()
+    {
+        AddingMachine tester = new AddingMachine();
+        assertEquals(-1, tester.subtract(1), "Subtracting 1 from the init should equal -1");
+        assertEquals(-2, tester.subtract(1), "Subtracting 1 from the init should equal -2");
+    }
+}
